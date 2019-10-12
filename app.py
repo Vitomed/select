af73fedd19fa3e694e97e54f08c0dfdf83378ef5
Vitomed <viktor.medyankin@mail.ru>
2019-10-12 16:11:48 +0300

    second commit

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def login():
     return "<h1>Statr Flask</h1>"
 
+@app.route("/index")
+def index():
+    return "<h1>Hello!</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=777)
