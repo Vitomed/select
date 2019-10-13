2a8c8e895f45cac5e75114fdcf6c73d82b11542d
Vitomed <viktor.medyankin@mail.ru>
2019-10-13 14:42:56 +0300

    3 commit

--- app.py
+++ app.py
@@ -1,16 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import datetime
+import json
+from datetime import datetime
+import random
 
 
 app = Flask(__name__)
 
 
-@app.route("/login")
-def login():
-    return "<h1>Statr Flask</h1>"
+@app.route('/1/<position_book>/<user>/1')
+def get_index(position_book, user):
+    with open('my_db.json', 'r', encoding='utf-8') as read_book:
+        read_book = json.load(read_book)
+        print("Position_book: ", position_book, "User: ", user)
+        print(read_book[1])
+        print("pass")
+        position_book = int(position_book)
+        status = read_book[1][position_book]["status"]
+        print(status)
+        if read_book[1][position_book]["status"] == "True":
+            if read_book[1][position_book]["count_now"] > 0:
+                user = int(user)
+                read_book[1][position_book]["count_now"] -= 1
+                read_book[2][user]["id_book"].append(read_book[1][position_book]["id_book"])
+                read_book[2][user]["id_user"].extend(read_book[2][user]["id_user"])
 
-@app.route("/index")
-def index():
-    return "<h1>Hello!</h1>"
+                time = datetime.now().strftime('%d.%m.%y')
+                read_book[2][user]["time_on"].append(time)
+            else:
+                read_book[1][position_book]["status"] = "False"
+
+        return render_template("index.html")
+
+
+@app.route('/action', methods=['POST'])
+def get_success():
+
+    #     with open('my_db.json', 'r', encoding='utf-8') as read_book:
+    #         read_book = json.load(read_book)
+    #
+    #         mail = request.form['mail']
+    #         user = random.randint(1, 2)
+    #
+    #         read_book[0][user]["knigaVzial"].extend(mail)
+    #
+    #         time = datetime.now().strftime('%d.%m.%y')
+    #         read_book[2][user]["time_on"].append(time)
+    #         # read_book[1][position_book]["count_now"] -= 1
+    #     return render_template("getsuccess.html")
+    return render_template("getsuccess.html")
+
+
+@app.route("/")
+def get_library():
+    return render_template("library.html")
+
+@app.route("/org")
+def get_org():
+    return render_template("org.html")
+
+@app.route("/putsucces")
+def get_putsucces():
+    return render_template("putsucces.html")
 
 
 if __name__ == "__main__":
